7b64c1bd4306d17ef216460e8a119d1b7ee828ad
kafukaiiilove <coolkafuka@163.com>
2024-11-14 16:48:44 +0800

    mesages

--- Test.c
+++ Test.c
@@ -1,11 +1,27 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
-    for (int i = 0; i < 10; i++)
-        printf("Hello Grayson~%d\n", i); 
- system("pause");
- system("anyways");
+int main() {
+    int n;
+    
+    // 输入三角形的行数
+    printf("请输入行数：");
+    scanf("%d", &n);
+    
+    // 打印等腰三角形
+    for (int i = 1; i <= n; i++) {
+        // 打印空格
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        
+        // 打印星号
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        
+        // 换行
+        printf("\n");
+    }
+    
     return 0;
 }
